92a4f54304b5ae009ec31fd977f3a92ac9f978df
Keisuke Ito <ito@komatsuelec.co.jp>
2015-01-08 18:06:18 +0900

    finish rss20

---
+++ chapture3/rss20.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+require 'nokogiri'
+require 'open-uri'
+
+url =
+  'http://www.amazon.co.jp/gp/rss/bestsellers/books/ref=zg_bs_books_rsslink'
+xml = open(url).read
+
+doc = Nokogiri::XML(xml)
+
+items = doc.xpath('//rss/channel/item')
+items.each do |item|
+  # タイトルを表示
+  puts item.xpath('title').text
+end
